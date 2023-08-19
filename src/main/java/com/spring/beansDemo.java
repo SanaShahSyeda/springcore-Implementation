6f422f7803c2235c94fa9d1b321c2b3fbb40a851
Syeda Sana <sana.shah1740@gmail.com>
2023-08-19 13:27:46 -0700

    created javabean POJO class.

---
+++ src/main/java/com/spring/beansDemo.java
@@ -0,0 +1,50 @@
+package com.spring;
+
+public class beansDemo {
+    private int id;
+    private String name;
+    private String address;
+
+    public beansDemo() {
+    }
+
+    public beansDemo(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", address='" + getAddress() + "'" +
+                "}";
+    }
+
+}
