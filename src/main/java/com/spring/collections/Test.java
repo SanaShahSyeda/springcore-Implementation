4851251d0d93a526d39a0ad52364a643057d7b3f
Syeda Sana <sana.shah1740@gmail.com>
2023-08-24 10:34:15 -0700

    setter injection through object data types.

---
+++ src/main/java/com/spring/collections/Test.java
@@ -0,0 +1,17 @@
+package com.spring.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "com/spring/collections/collectionsconfig.xml");
+        emp emp1 = (emp) context.getBean("emp1");
+        System.out.println(emp1);
+    }
+
+}
---
+++ src/main/java/com/spring/collections/emp.java
@@ -0,0 +1,64 @@
+package com.spring.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class emp {
+    private String name;
+    private List<String> phones;
+    private Set<String> addresses;
+    private Map<String, String> courses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
+    }
+
+    public Set<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<String> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Map<String, String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Map<String, String> courses) {
+        this.courses = courses;
+    }
+
+    public emp(String name, List<String> phones, Set<String> addresses, Map<String, String> courses) {
+        super();
+        this.name = name;
+        this.phones = phones;
+        this.addresses = addresses;
+        this.courses = courses;
+    }
+
+    public emp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String toString() {
+        return "emp [name=" + name + ", \nphones=" + phones + ",\naddresses=" + addresses + ", \ncourses=" + courses
+                + "]";
+    }
+
+}
