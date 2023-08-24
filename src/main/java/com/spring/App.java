ea8c233475dc4516f016afa6ae22c5131198c435
Syeda Sana <sana.shah1740@gmail.com>
2023-08-24 10:14:49 -0700

    some bug fixing and print values through setter injection.

--- src/main/java/com/spring/App.java
+++ src/main/java/com/spring/App.java
@@ -1,11 +1,13 @@
 package com.spring;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        beansDemo student1 = (beansDemo) context.getBean("Student");
+        System.out.println(student1);
     }
 }
