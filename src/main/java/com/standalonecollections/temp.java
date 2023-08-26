8f6f98c5d08e9073bca84ebcec2561f5e9d1d284
Syeda Sana <sana.shah1740@gmail.com>
2023-08-26 05:38:08 -0700

    configured stand-alone "Properties" & "HashMap" to be injected.

--- src/main/java/com/standalonecollections/temp.java
+++ src/main/java/com/standalonecollections/temp.java
@@ -1,11 +1,29 @@
 package com.standalonecollections;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class temp {
     private List<String> friends;
+    private Map<String, Integer> feestructure;
+    private Properties properties;
 
     public temp() {
+    }
+
+    public temp(List<String> friends, Map<String, Integer> feestructure, Properties properties) {
+        this.friends = friends;
+        this.feestructure = feestructure;
+        this.properties = properties;
+    }
+
+    public Map<String, Integer> getFeestructure() {
+        return this.feestructure;
+    }
+
+    public void setFeestructure(Map<String, Integer> feestructure) {
+        this.feestructure = feestructure;
     }
 
     public List<String> getFriends() {
@@ -16,10 +34,20 @@ public class temp {
         this.friends = friends;
     }
 
+    public Properties getProperties() {
+        return this.properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
     @Override
     public String toString() {
         return "{" +
                 " friends='" + getFriends() + "'" +
+                " feestructure='" + getFeestructure() + "'" +
+                " properties='" + getProperties() + "'" +
                 "}";
     }
 
--- src/main/java/com/standalonecollections/testTemp.java
+++ src/main/java/com/standalonecollections/testTemp.java
@@ -5,13 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class testTemp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 "com/standalonecollections/configstandalone.xml");
         temp ob = context.getBean("person", temp.class);
         System.out.println(ob);
         System.out.println(ob.getFriends().getClass().getName());
-
+        System.out.println(ob.getFeestructure().getClass().getName());
+        System.out.println(ob.getProperties().getClass().getName());
     }
 }
